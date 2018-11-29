c30bd3f70ccc0caa73377706c7ed49c4f28d20bd
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2018-11-29 12:18:00 +0300

    minor fixes

--- interact_midi.py
+++ interact_midi.py
@@ -2,32 +2,40 @@ import interact_debug
 import preproc
 
 import glob
-import os
+
 
 
 def bootstrap():
 
 
     try:
+
         file = glob.glob("*.mid")[-1]
         print(f'Obtained file: {file}')
+
     except Exception as e:
+
         print('Error : No .mid files found.')
         file = None
         # file = os.path.abspath(input('Drag & Drop midi file: '))
 
     try:
+
         if file is not None:
             print('Processing data..')
             data = preproc.midi_to_stream(file)
-        else: data = None
+
+        else: data = []
+
     except Exception as e:
+
         data = []
         print('Error : Provided midi cannot be processed.',e)
 
     try:
+
         if len(data) > 0:
-            
+
             print('Asking ai..')
             response = interact_debug.bootstrap(data)
 
@@ -36,8 +44,13 @@ def bootstrap():
                 for stuff in resp_step:
                     print(stuff)
                 print("-----")
+
         else: print('Error : No midi information was extracted.')
+
     except Exception as e: print('Error : No response could be generated.',e)
+
+
+
 
 
 if __name__ == '__main__':
